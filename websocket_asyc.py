f65c5d1f50ab038bc6153b0904a072ee937b70e1
lum1pyry12122017 <villenposti75g@gmail.com>
2023-11-25 00:01:27 +0200

    working version, message per request

--- websocket_asyc.py
+++ websocket_asyc.py
@@ -6,18 +6,19 @@ class CsvWebSocketServer():
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.path = "./data_folder/"
         
     # read csv
     def read_csv(self, file_number: str, line_number: int):
-        filename = f"data{file_number}.csv"
-        print(f"Yritetään avata tiedostoa: {filename}")  # Debuggausviesti
+        filename = self.path + f"data{file_number}.csv"
+        # print(f"Avataan tiedosto: {filename}")
 
         try:
             with open(filename, newline="") as csv_file:
                 reader = csv.reader(csv_file)
                 result = []
                 for i, row in enumerate(reader):
-                    print(f"Rivi {i}: {row}")  # Tulostaa rivinumeron ja rivin
+                    # print(f"Rivi {i}: {row}")  # Tulostaa rivinumeron ja rivin
                     if row and int(row[0]) == line_number:
                         result.append(row)
                 return result
@@ -28,24 +29,26 @@ class CsvWebSocketServer():
             print("Virhe muunneltaessa rivin ensimmäistä saraketta kokonaisluvuksi.")
             return None
 
-    # websoket server
+    # websoket server, request from client to this server
     async def ws_handler(self, websocket, path):
         async for message in websocket:
             try:
                 file_number, file_name, line_number = message.split(",")
                 file_number = int(file_number)
+                file_name = str(file_name)
                 line_number = int(line_number)
 
-                # check content
+                # check content of request, if true read file
                 if 0 <= file_number <= 5 and 0 <= line_number <= 360:
+                    # call read file
                     data= self.read_csv(file_number,line_number)
-                    print(data)
+                    print("Data from file: ", data)
                     if data:
                         await websocket.send(str(data))
                     else:
-                        await websocket.send("Tiedostoa ei löydy")
+                        await websocket.send("File not found")
                 else:
-                    await websocket.send("Viheellinen pyyntö")
+                    await websocket.send("Invalid request")
             except ValueError:
                 await websocket.send("Virheelinen viesti")
     # palvelimen käynnistys
